3bd264b5f198c109af9b0857fe37882a19f2505a
Ezra <115100554+eb73@users.noreply.github.com>
2023-10-19 13:43:50 +0200

    principal fcts from helpers to implementation
    also deleted the copies of the functions from Functions.ipynb

--- implementations.py
+++ implementations.py
@@ -5,8 +5,6 @@ import numpy as np
 # all functions should return: (w, loss), which is the last weight vector of the
 #method, and the corresponding loss value (cost function).
 
-# change so we don't keep all weights and losses in memory (bc big dataset)
-
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """Linear regression using gradient descent
 
@@ -17,9 +15,20 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
         max_iters: scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
     """
-    weights, losses = gradient_descent(y, tx, initial_w, max_iters, gamma)
-    w = weights[-1]
-    loss = losses[-1]
+    # initialize w
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute gradient
+        grad, err = compute_gradient(y, tx, w)
+        # update w by gradient descent
+        w = w - gamma * grad
+    """if we want the details of the convergence
+        loss = calculate_mse(err)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    """
+    # compute the final loss
+    loss = calculate_mse(err)
     return w, loss
     
 
@@ -33,9 +42,20 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
         max_iters: scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
     """
-    weights, losses = stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma)
-    w = weights[-1]
-    loss = losses[-1]
+    w = initial_w
+    for n_iter in range(max_iters):
+        # we use a batch_size of 1 (as stated in the project description)
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
+            # compute a stochastic gradient
+            grad, _ = compute_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+        """ if we want the details of the convergence, uncomment this part
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l= compute_loss(y, tx, w), w0=w[0], w1=w[1]))
+        """
+    # compute the final loss
+    loss = compute_loss(y, tx, w)
     return w, loss
 
 
@@ -63,9 +83,9 @@ def ridge_regression(y, tx, lambda_):
         lambda_: regularization parameter
     """
     aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
-    a = tx.T.dot(tx) + aI
+    A = tx.T.dot(tx) + aI
     b = tx.T.dot(y)
-    w = np.linalg.solve(a, b)
+    w = np.linalg.solve(A, b)
     loss = compute_loss(y,tx,w)
     return w, loss
 
@@ -79,12 +99,22 @@ def logistic_regression_GD(y, tx, initial_w, max_iters, gamma):
         max_iters: scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
     """
-    weights, losses = GD_logistic(y, tx, initial_w, max_iters, gamma)
-    w = weights[-1]
-    loss = losses[-1]
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute gradient
+        grad, err = compute_gradient_logistic(y, tx, w)
+        # update w by gradient descent
+        w = w - gamma * grad
+        """if want details of the convergence
+        loss = compute_loss_logistic(y, tx , w)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        """
+    #compute final loss
+    loss = compute_loss_logistic(y, tx, w)
     return w, loss
    
-def logistic_regression_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
+def logistic_regression_SGD(y, tx, initial_w, max_iters, gamma):
     """Logistic regression using SGD (y in {0,1})
 
     Args:
@@ -95,9 +125,21 @@ def logistic_regression_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
         gamma: a scalar denoting the stepsize
         batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
     """
-    weights, losses = SGD_logistic(y, tx, initial_w, batch_size, max_iters, gamma)
-    w = weights[-1]
-    loss = losses[-1]
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = compute_gradient_logistic(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad
+        """    
+        loss = compute_loss_logistic(y, tx, w)
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        """
+    #compute final loss
+    loss = compute_loss_logistic(y, tx, w)
     return w, loss
 
 def regularized_logistic_regression_GD(y, tx, initial_w, max_iters, gamma, lambda_):
@@ -112,12 +154,23 @@ def regularized_logistic_regression_GD(y, tx, initial_w, max_iters, gamma, lambd
         gamma: a scalar denoting the stepsize
         lambda_: regularization parameter
     """
-    weights, losses = Regularized_GD_logistic(y, tx, initial_w, max_iters, gamma, lambda_)
-    w = weights[-1]
-    loss = losses[-1]
+    w = initial_w
+    for n_iter in range(max_iters):
+        # compute gradient
+        grad, err = regularized_logistic_gradient(y, tx, w, lambda_)
+        # update w by gradient descent
+        w = w - gamma * grad
+        # store w and loss
+    """print details
+        loss = Regularized_logistic_loss(y, tx , w, lambda_)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+    """
+    #compute final loss
+    loss = regularized_logistic_loss(y, tx, w, lambda_)
     return w, loss
 
-def regularized_logistic_regression_SGD(y, tx, initial_w, batch_size, max_iters, gamma, lambda_):
+def regularized_logistic_regression_SGD(y, tx, initial_w, max_iters, gamma, lambda_):
     """Regularized logistic regression using SGD (y in {0,1},
     regularization term lambda*||w||^2)
 
@@ -130,7 +183,19 @@ def regularized_logistic_regression_SGD(y, tx, initial_w, batch_size, max_iters,
         gamma: a scalar denoting the stepsize
         lambda_: regularization parameter
     """
-    weights, losses = Regularized_SGD_logistic(y, tx, initial_w, batch_size, max_iters, gamma, lambda_)
-    w = weights[-1]
-    loss = losses[-1]
+    w = initial_w
+
+    for n_iter in range(max_iters):
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size=1, num_batches=1):
+            # compute a stochastic gradient and loss
+            grad, _ = regularized_logistic_gradient(y_batch, tx_batch, w)
+            # update w through the stochastic gradient update
+            w = w - gamma * grad    
+        """print details
+        # calculate loss
+        loss = Regularized_logistic_loss(y, tx, w)
+        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+        """        
+    loss = regularized_logistic_loss(y, tx, w)
     return w, loss
